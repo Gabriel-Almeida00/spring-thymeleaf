a6bd001e459895ba0f2f4b2f1b36264c9b4f2474
gabriel <gaamdal557@gmail.com>
2023-01-27 21:59:43 -0300

    arrumando erro edtar departamento

--- src/main/java/com/gabriel/demomvc/controller/DepartamentoController.java
+++ src/main/java/com/gabriel/demomvc/controller/DepartamentoController.java
@@ -35,7 +35,7 @@ public class DepartamentoController {
     }
 
     @GetMapping("/editar/{id}")
-    public String preEditar(@PathVariable ("id") Long id,ModelMap model) {
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
     model.addAttribute("departamento",service.buscarPorId(id));
     return "/departamento/cadastro";
     }
