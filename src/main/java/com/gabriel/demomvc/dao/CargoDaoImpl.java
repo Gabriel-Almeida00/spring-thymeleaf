9e3036063771dc8bbfe78c1a74ebfde58ac70fde
gabriel <gaamdal557@gmail.com>
2023-02-14 14:27:26 -0300

    preparando a consulta paginada

--- src/main/java/com/gabriel/demomvc/dao/CargoDaoImpl.java
+++ src/main/java/com/gabriel/demomvc/dao/CargoDaoImpl.java
@@ -1,6 +1,7 @@
 package com.gabriel.demomvc.dao;
 
 import com.gabriel.demomvc.domain.Cargo;
+import com.gabriel.demomvc.util.PaginacaoUtil;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,5 +9,16 @@ import java.util.List;
 
 @Repository
 public  class CargoDaoImpl extends AbstractDao<Cargo, Long> implements  CargoDao{
+
+    public PaginacaoUtil<Cargo> buscaPaginada( int pagina){
+        int tamanho = 5;
+        int inicio = (pagina - 1) * tamanho;
+        List<Cargo> cargos = getEntityManager()
+                .createQuery("select c from Cargo c order by c.nome asc", Cargo.class)
+                .setFirstResult(inicio)
+                .setMaxResults(tamanho)
+                .getResultList();
+        return new PaginacaoUtil<>(tamanho, pagina, totalDePaginas, cargos);
+    }
 
 }
